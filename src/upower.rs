18113220a7593cdabac837c4e41fa871eb7928ea
Michael Gruenewald <mail@michaelgruenewald.eu>
2024-08-03 11:32:53 +0200

    Bump dependencies again

--- src/upower.rs
+++ src/upower.rs
@@ -1,7 +1,7 @@
 use zbus::zvariant::{ObjectPath, OwnedObjectPath};
-use zbus::{dbus_proxy, Result};
+use zbus::{proxy, Result};
 
-#[dbus_proxy(
+#[proxy(
     default_service = "org.freedesktop.UPower",
     interface = "org.freedesktop.UPower",
     default_path = "/org/freedesktop/UPower"
@@ -9,18 +9,18 @@ use zbus::{dbus_proxy, Result};
 trait UPower {
     fn enumerate_devices(&self) -> Result<Vec<OwnedObjectPath>>;
 
-    #[dbus_proxy(signal)]
+    #[zbus(signal)]
     fn device_added(&self, path: ObjectPath<'_>) -> Result<()>;
 }
 
-#[dbus_proxy(
+#[proxy(
     default_service = "org.freedesktop.UPower",
     interface = "org.freedesktop.UPower.Device"
 )]
 trait UPowerDevice {
-    #[dbus_proxy(property)]
+    #[zbus(property)]
     fn native_path(&self) -> Result<String>;
 
-    #[dbus_proxy(property)]
+    #[zbus(property)]
     fn percentage(&self) -> Result<f64>;
 }
